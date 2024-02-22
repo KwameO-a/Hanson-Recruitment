a1d8e5f112f4249c09b1f7559cbcb43efd920ce4
Cyril26 <cyrilodm26@gmail.com>
2024-02-22 16:30:37 +0000

    styling

--- my-app/ta-application-form/src/components/PersonalDetails/PersonalDetailsForm.tsx
+++ my-app/ta-application-form/src/components/PersonalDetails/PersonalDetailsForm.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { TextField, MenuItem, Button, Typography, Checkbox, FormControlLabel} from '@mui/material';
+import { TextField, MenuItem, Button, Typography, Checkbox, FormControlLabel, Box, useMediaQuery } from '@mui/material';
 import PhoneInput from 'react-phone-number-input';
 import 'react-phone-number-input/style.css';
 import { countries } from 'countries-list';
 
 const countryOptions = Object.entries(countries).map(([code, { name }]) => ({
-    label: name,
-    value: code,
+  label: name,
+  value: code,
 }));
-  
-  
+
+
 
 interface PersonalDetailsProps {
   onNext: () => void; // Function to navigate to the next form section
@@ -28,10 +28,11 @@ const PersonalDetails: React.FC<PersonalDetailsProps> = ({ onNext }) => {
   const [yourEmail, setYourEmail] = useState('');
   const [dateOfBirth, setDateOfBirth] = useState('');
   const [townOfBirth, setTownOfBirth] = useState('');
-  const [selectedNationality, setSelectedNationality] = useState(''); 
+  const [selectedNationality, setSelectedNationality] = useState('');
   const [nationalInsurance, setNationalInsurance] = useState('');
   const [gender, setGender] = useState('');
-  const [dbsCheck, setDbsCheck] = useState({needNewDBS: false,haveDBSOnUpdateService: false,});
+  const [dbsCheck, setDbsCheck] = useState({ needNewDBS: false, haveDBSOnUpdateService: false, });
+  const isNonMobileScreens = useMediaQuery("(min-width: 600px)");
 
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, checked } = event.target;
@@ -40,95 +41,116 @@ const PersonalDetails: React.FC<PersonalDetailsProps> = ({ onNext }) => {
       haveDBSOnUpdateService: name === "haveDBSOnUpdateService" ? checked : false,
     });
   };
-  
-
-  
 
   return (
-    <form>
-        <Typography variant="h5" component="h2" gutterBottom style={{ fontWeight: 'bold' }}>
-      Personal Details
-    </Typography>
-       
-      {/* Title dropdown */}
-      <TextField select label="Title" value={title} onChange={(e) => setTitle(e.target.value)} fullWidth margin="normal">
-        <MenuItem value="Dr.">Dr.</MenuItem>
-        <MenuItem value="Mr.">Mr.</MenuItem>
-        <MenuItem value="Mrs.">Mrs.</MenuItem>
-        <MenuItem value="Ms.">Ms.</MenuItem>
-        {/* Add more options as needed */}
-      </TextField>
-      {/* Text fields for personal information */}
-      <TextField label="Legal First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} fullWidth margin="normal" />
-      <TextField label="Legal Middle Name(s)" value={middleName} onChange={(e) => setMiddleName(e.target.value)} fullWidth margin="normal" />
-      <TextField label="Legal Surname" value={lastName} onChange={(e) => setLastName(e.target.value)} fullWidth margin="normal" />
-      <TextField label="Known as" value={knownAs} onChange={(e) => setKnownAs(e.target.value)} fullWidth margin="normal" />
-      <TextField label="Previous Names" value={previousNames} onChange={(e) => setPreviousNames(e.target.value)} fullWidth margin="normal" />
-      <TextField label="Address" value={address} onChange={(e) => setAddress(e.target.value)} fullWidth margin="normal" />
-      <TextField label="Postcode" value={postcode} onChange={(e) => setPostcode(e.target.value)} fullWidth margin="normal" />
+    <Box>
+      <Box
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Personal Details
+        </Typography>
+      </Box>
 
-      <PhoneInput
-        international
-        defaultCountry="GB"
-        value={phoneNumber}
-        onChange={(value) => setPhoneNumber(value || '')}
-        style={{ width: '100%', margin: '16px 0', }}
-      />
-      <TextField label="Your email" value={yourEmail} onChange={(e) => setYourEmail(e.target.value)} fullWidth margin="normal" />
-      
-      
-      <TextField type="date" label="Date of Birth" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} InputLabelProps={{ shrink: true, }} fullWidth margin="normal" />
-      <TextField label="Town of Birth" value={townOfBirth} onChange={(e) => setTownOfBirth(e.target.value)} fullWidth margin="normal" />
-      <TextField
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="1rem"
+        m="1rem auto"
+        borderRadius="1.5rem"
+      >
+        <form>
+          {/* Title dropdown */}
+          <TextField select label="Title" value={title} onChange={(e) => setTitle(e.target.value)} fullWidth margin="normal">
+            <MenuItem value="Dr.">Dr.</MenuItem>
+            <MenuItem value="Mr.">Mr.</MenuItem>
+            <MenuItem value="Mrs.">Mrs.</MenuItem>
+            <MenuItem value="Ms.">Ms.</MenuItem>
+            {/* Add more options as needed */}
+          </TextField>
+          {/* Text fields for personal information */}
+          <TextField label="Legal First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} fullWidth margin="normal" />
+          <TextField label="Legal Middle Name(s)" value={middleName} onChange={(e) => setMiddleName(e.target.value)} fullWidth margin="normal" />
+          <TextField label="Legal Surname" value={lastName} onChange={(e) => setLastName(e.target.value)} fullWidth margin="normal" />
+          <TextField label="Known as" value={knownAs} onChange={(e) => setKnownAs(e.target.value)} fullWidth margin="normal" />
+          <TextField label="Previous Names" value={previousNames} onChange={(e) => setPreviousNames(e.target.value)} fullWidth margin="normal" />
+          <TextField label="Address" value={address} onChange={(e) => setAddress(e.target.value)} fullWidth margin="normal" />
+          <TextField label="Postcode" value={postcode} onChange={(e) => setPostcode(e.target.value)} fullWidth margin="normal" />
+
+          <PhoneInput
+            international
+            defaultCountry="GB"
+            value={phoneNumber}
+            onChange={(value) => setPhoneNumber(value || '')}
+            style={{ width: '100%', margin: '16px 0', }}
+          />
+          <TextField label="Your email" value={yourEmail} onChange={(e) => setYourEmail(e.target.value)} fullWidth margin="normal" />
+
+
+          <TextField type="date" label="Date of Birth" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} InputLabelProps={{ shrink: true, }} fullWidth margin="normal" />
+          <TextField label="Town of Birth" value={townOfBirth} onChange={(e) => setTownOfBirth(e.target.value)} fullWidth margin="normal" />
+          <TextField
             select
             label="Nationality"
             value={selectedNationality}
             onChange={(e) => setSelectedNationality(e.target.value)}
             fullWidth
             margin="normal"
-            >
+          >
             {countryOptions.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
+              <MenuItem key={option.value} value={option.value}>
                 {option.label}
-                </MenuItem>
+              </MenuItem>
             ))}
-        </TextField>
-        <TextField label="National Insurance No." value={nationalInsurance} onChange={(e) => setNationalInsurance(e.target.value)} fullWidth margin="normal" />
-        <TextField select label="Gender" value={gender} onChange={(e) => setGender(e.target.value)} fullWidth margin="normal">
-        <MenuItem value="Female">Female</MenuItem>
-        <MenuItem value="Male">Male</MenuItem>
-        {/* Add more options as needed */}
-      </TextField>
-      <Typography variant="body1" gutterBottom>
-        If Hanson Recruitment are completing a new DBS for you, it will be Child and Adult Workforce. Do you need a new DBS or do you have one (child workforce/ child & adult workforce) on the update service?
-      </Typography>
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={dbsCheck.needNewDBS}
-            onChange={handleCheckboxChange}
-            name="needNewDBS"
+          </TextField>
+          <TextField label="National Insurance No." value={nationalInsurance} onChange={(e) => setNationalInsurance(e.target.value)} fullWidth margin="normal" />
+          <TextField select label="Gender" value={gender} onChange={(e) => setGender(e.target.value)} fullWidth margin="normal">
+            <MenuItem value="Female">Female</MenuItem>
+            <MenuItem value="Male">Male</MenuItem>
+            {/* Add more options as needed */}
+          </TextField>
+          <Typography variant="body1" gutterBottom sx={{ m: "1.5rem", letterSpacing: "0.7px" }}  >
+            If Hanson Recruitment are completing a new DBS for you, it will be Child and Adult Workforce. Do you need a new DBS or do you have one (child workforce/ child & adult workforce) on the update service?
+          </Typography>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={dbsCheck.needNewDBS}
+                onChange={handleCheckboxChange}
+                name="needNewDBS"
+              />
+            }
+            label="No, I need a new DBS"
           />
-        }
-        label="No, I need a new DBS"
-      />
-       <FormControlLabel
-        control={
-          <Checkbox
-            checked={dbsCheck.haveDBSOnUpdateService}
-            onChange={handleCheckboxChange}
-            name="haveDBSOnUpdateService"
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={dbsCheck.haveDBSOnUpdateService}
+                onChange={handleCheckboxChange}
+                name="haveDBSOnUpdateService"
+              />
+            }
+            label="Yes, I already have an enhance child and adult workforce one on the update service"
           />
-        }
-        label="Yes, I already have an enhance child and adult workforce one on the update service"
-      />
-      <Typography variant="body2" gutterBottom>
-        If you have a DBS on the update service, we will need a copy of the DBS sent to us, and the physical copy must be presented to the schools when you go in for work.
-      </Typography>
+          <Typography fontWeight="500" variant="body1" sx={{ m: "1.5rem", letterSpacing: "0.7px" }} gutterBottom>
+            If you have a DBS on the update service, we will need a copy of the DBS sent to us, and the physical copy must be presented to the schools when you go in for work.
+          </Typography>
 
 
-      <Button variant="contained" onClick={onNext}>Next</Button>
-    </form>
+          <Button 
+            variant="contained"
+            onClick={onNext}
+            fullWidth
+            type="submit"
+            sx={{
+              m: "2rem 0",
+              p: "1rem",
+            }}>
+              Next
+          </Button>
+        </form>
+      </Box>
+    </Box>
   );
 };
 
